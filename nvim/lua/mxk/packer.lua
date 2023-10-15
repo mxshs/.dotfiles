774e80057012d95e8de28fe3411ddc95e2a294df
mxk <mmshshkv@gmail.com>
2023-10-15 20:53:06 +0300

    feat: add undotree/fugitive to nvim conf

--- nvim/lua/mxk/packer.lua
+++ nvim/lua/mxk/packer.lua
@@ -20,6 +20,8 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
     use {
         'VonHeikemen/lsp-zero.nvim',
